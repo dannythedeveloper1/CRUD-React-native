bf806f6440cce4ac2a4170d6eea92799dad9448a
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2021-11-12 12:53:13 -0500

    add(feat): add and delete functionality

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, Image, View, FlatList, Alert } from 'react-native';
+import { v4 as uuid } from 'uuid';
+import AddItem from './components/AddItem';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 export default function App() {
+  const [items, setItems] = useState([
+    { id: uuid(), text: "milk" },
+    { id: uuid(), text: "Eggs" },
+    { id: uuid(), text: "Bread" },
+    { id: uuid(), text: "Juice" },
+  ]);
+
+  const addItem = (text) => {
+    console.log(text);
+    if (!text) {
+      alert('please enter an item');
+      // Alert.alert('Error', 'Please enter an item', { text: 'Ok' })
+    } else {
+      setItems([{ id: uuid(), text }, ...items]);
+    }
+  }
+
+  const deleteItem = (id) => {
+    const newItems = items.filter(item => item.id !== id);
+    setItems(newItems);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header title='Shopping List' />
+      <AddItem addItem={addItem} />
+      <FlatList data={items}
+        renderItem={({ item }) => <ListItem item={item} deleteItem={deleteItem} />}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +44,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: 60
+  }
 });
---
+++ components/AddItem.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, TouchableOpacity, Text } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+const AddItem = ({addItem}) => {
+    const [text, setText] = useState("");
+    return (
+        <View>
+            <TextInput placeholder="Add Item..." style={styles.input} onChangeText={(e)=>setText(e)}/>
+            <TouchableOpacity style={styles.btn} onPress={()=>addItem(text)}>
+                <Text style={styles.btnText}><FontAwesome name="plus" size={20} color="darkslateblue"/>Add Item</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        height: 60,
+        padding: 8,
+        fontSize: 16,
+    },
+    btn: {
+        backgroundColor: "#c2bad8",
+        padding: 9,
+        fontSize: 16
+    },
+    btnText: {
+        color: "darkslateblue",
+        fontSize: 20,
+        textAlign: "center",
+    },
+})
+
+export default AddItem;
